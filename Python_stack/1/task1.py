8cc0bd6436a0edbb7341c06e1db30f16cefcafa3
Darkvran <darkvran2019@gmail.com>
2026-02-11 17:53:47 +0300

    Some coments expanded

--- Python_stack/1/task1.py
+++ Python_stack/1/task1.py
@@ -1,18 +1,35 @@
 """
+RU:
+Давай представим, что мы пишем бэкенд для сайта ветеринарной клиники.
+Нам нужно написать программу, которая будет запрашивать у пользователя вид питомца, 
+его возраст и кличку, а потом выведет все эти данные в одно предложение. Например:
+Это желторотый питон по кличке "Каа". Возраст: 34 года.
+
+EN:
 Imagine we are writing the backend for a veterinary clinic website.
 We need to write a program that will ask the user for the type of pet,
 its age, and its name, and then output all this information in a single sentence. For example:
 This is a yellow-bellied python named "Kaa". Age: 34 years.
 """
 
+#RU: Более простой, но неинтересный вариант решения. 
+#EN: More simplier, but boring solution.
 # petType = input("Введите вид питомца\n")
 # petName = input("Введите имя питомца\n")
 # petAge = input("Введите возраст питомца\n")
 # print(f"Это {petType} по кличке \"{petName}\". Возраст: {petAge}.")
 
+#EN:
 # In future versions we can make more flex age attribution.
-# (We can make it with user-input date of birth insead and data).
-# Also we can make age filters.
+# (We can make it with user-input date of birth insead of age and using datetime module).
+# Also we can make age filters. (Age is delta between cur. data and date of birth)
+
+#RU:
+#В будущих версиях мы можем сделать более гибкий атрибут возраста.
+# (Это можно сделать с помощью ввода от пользователя даты рождения питомца заместо возраста
+# и использования модуля datetime)
+# Также можно будет добавить фильтры возраста.
+# (Возраст - дельта между текущей датой и датой рождения.)
 
 class Pet:
     """
--- Python_stack/1/task2.py
+++ Python_stack/1/task2.py
@@ -1,4 +1,8 @@
 """
+RU:
+В этом задании необходимо вывести порядок стадий эволюций человека, разделенных знаком "=>".
+
+EN:
 In this task we should output the order of stages human evolution, separated with "=>" sign
 """
 
